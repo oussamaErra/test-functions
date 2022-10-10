0166bd3de1c33d460ea1b5bd276c59b07b4e542e
Oussama <oussama@cleverppc.com>
2022-10-10 14:48:25 +0100

    cahnge main

---
+++ main_backup.py
@@ -0,0 +1,35 @@
+import os
+import pandas as pd
+import logging
+
+from sklearn.datasets import make_classification
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+import numpy as np
+
+LOG = logging.getLogger()
+
+
+def test_function():
+    print("TEST DUMP 4")
+    print(os.listdir())
+
+def trigger():
+    LOG.info("shape of data is : {}")
+    data = make_classification(n_samples=10000,n_features=5,n_classes=2)
+    data = pd.DataFrame(np.column_stack(data))
+    LOG.info(f"shape of data is : {data.shape}")
+    data.columns =["a","b","c","d","e","target"]
+    model  = RandomForestClassifier()
+    model.fit(data.drop("target",axis=1),data["target"])
+
+    pred = model.predict(data.drop("target",axis=1))
+
+    acc = accuracy_score(data["target"],pred)
+
+    print(acc)
+    LOG.info(f"ACCURACY : {acc} ")
+
+
+
